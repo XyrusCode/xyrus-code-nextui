eee8decf50e46a0ed3538ea66367e9325ec8acae
Prince Shammah <shammahprinz@gmail.com>
2024-01-09 12:07:51 +0100

    fix: check if now playing is valid

--- app/api/now-playing/route.ts
+++ app/api/now-playing/route.ts
@@ -2,36 +2,51 @@ import { getNowPlaying } from '@/lib/spotify';
 import type { NowPlayingResponse,NowPlayingSong } from '@/types/spotify';
 
 export const GET = async () => { 
-	const request = await getNowPlaying();
-	const response: NowPlayingResponse = await request.json();;
+	try {
+		const request = await getNowPlaying();
+		const response = await request.json();
 
-	const headers = {
-		'content-type': 'application/json',
-		'cache-control': 'public, s-maxage=60, stale-while-revalidate=30'
-	};
+		const headers = {
+			'content-type': 'application/json',
+			'cache-control': 'public, s-maxage=60, stale-while-revalidate=30'
+		};
 
-	if (response === null) {
-		return new Response(JSON.stringify({ isPlaying: false }), {
-			status: 200,
-			headers: headers
-		});
-	} else {
-
-		return Response.json(
-			{
-				id: response.item.id,
-				isPlaying: response.is_playing,
-				title: response.item.name,
-				artist: response.item.artists.map((_artist: any) => _artist.name).join(', '),
-				album: response.item.album.name,
-				albumImageUrl: response.item.album.images[0].url,
-				songUrl: response.item.external_urls.spotify
-				
-			} as NowPlayingSong,
-			{
+		if (response === null) {
+			return new Response(JSON.stringify({ isPlaying: false }), {
 				status: 200,
 				headers: headers
+			});
+		} else {
+			// Check if the response is valid JSON
+			if (response && typeof response === 'object') {
+				return new Response(JSON.stringify({
+					id: response.item.id,
+					isPlaying: response.is_playing,
+					title: response.item.name,
+					artist: response.item.artists.map((_artist: any) => _artist.name).join(', '),
+					album: response.item.album.name,
+					albumImageUrl: response.item.album.images[0].url,
+					songUrl: response.item.external_urls.spotify
+				} as NowPlayingSong), {
+					status: 200,
+					headers: headers
+				});
+			} else {
+				// Handle the case where the response is not valid JSON
+				console.error('Invalid JSON response:', response);
+				return new Response(JSON.stringify({ error: 'Invalid JSON response' }), {
+					status: 500,
+					headers: headers
+				});
 			}
-		);
+		}
+	} catch (error) {
+		console.error('Error fetching now playing data:', error);
+		return new Response(JSON.stringify({ error: 'Error fetching now playing data' }), {
+			status: 500,
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
 	}
 };
